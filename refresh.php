0f7fe80c89a1df6461b3f3da4eb346488c64f3a8
Arno Richter <mail@arnorichter.de>
2023-03-27 23:00:24 +0200

    better error handling for missing config file, eg in first time setup

--- refresh.php
+++ refresh.php
@@ -1,5 +1,6 @@
 <?php
 
+if(!file_exists(__DIR__.DIRECTORY_SEPARATOR.'config.php')) die('Create a config.php according to <a href="README.md">README.md</a>');
 require_once(__DIR__.DIRECTORY_SEPARATOR.'config.php');
 
 $feed = [];
